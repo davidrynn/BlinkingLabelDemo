cf647b3665dc2ce4cd47aa0d8fe85637f97d0894
David Rynn <davidrynn@gmail.com>
2019-09-12 19:09:10 -0400

    initial app works

--- Example/BlinkingLabel/ViewController.swift
+++ Example/BlinkingLabel/ViewController.swift
@@ -7,31 +7,32 @@
 //
 
 import UIKit
+import BlinkingLabel
 
 class ViewController: UIViewController {
 
     var isBlinking = false
-    let blinkingLabel = BlinkingLabel(frame: CGRect(10, 20, 200, 30))
+    let blinkingLabel = BlinkingLabel(frame: CGRect(x: 10, y: 20, width: 200, height: 30))
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Setup the BlinkingLabel
         blinkingLabel.text = "I blink!"
-        blinkingLabel.font = UIFont.systemFontOfSize(20)
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20)
         view.addSubview(blinkingLabel)
         blinkingLabel.startBlinking()
         isBlinking = true
 
         // Create a UIButton to toggle the blinking
-        let toggleButton = UIButton(frame: CGRect(10, 60, 125, 30))
-        toggleButton.setTitle("Toggle Blinking", forState: .normal)
-        toggleButton.setTitleColor(UIColor.redColor(), forState: .normal)
-        toggleButton.addTarget(self, action: "toggleBlinking", forControlEvents: .touchUpInside)
+        let toggleButton = UIButton(frame: CGRect(x: 10, y: 60, width: 125, height: 30))
+        toggleButton.setTitle("Toggle Blinking", for: .normal)
+        toggleButton.setTitleColor(.red, for: .normal)
+        toggleButton.addTarget(self, action: #selector(toggleBlinking), for: .touchUpInside)
         view.addSubview(toggleButton)
     }
 
-    func toggleBlinking() {
+    @objc func toggleBlinking() {
         if (isBlinking) {
             blinkingLabel.stopBlinking()
         } else {
